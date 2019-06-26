e9a8a9cb975a259495cd3ec3bdf2c4f1e293e608
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-06-26 16:19:07 +0700

    feat(setup-app): initial commit

---
+++ app/src/main/java/com/example/movieguide/activities/SearchPageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.movieguide.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieguide.R
+
+class SearchPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
